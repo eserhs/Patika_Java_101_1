a3197d70ee1ac4d9581f24d587c6459e3f592b69
eserhs <huseyinbanna216@gmail.com>
2022-11-09 14:05:32 +0300

    Horoscope

---
+++ src/Java_101/Horoscope.java
@@ -0,0 +1,92 @@
+package Java_101;
+import java.util.Scanner;
+public class Horoscope {
+    public static void main(String[] args) {
+        int month, day;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Doğduğunuz Ay : ");
+        month = input.nextInt();
+
+        System.out.println("Doğduğunuz Gün : ");
+        day = input.nextInt();
+
+        do {
+            if (day > 30) {
+                System.out.print("Lütfen 1-30 arasında bir sayı giriniz: ");
+                day = input.nextInt();
+            }
+
+            if (day < 1) {
+                do {
+                    System.out.print("Lütfen 1-30 arasında bir sayı giriniz: ");
+                    day = input.nextInt();
+                } while (day < 1);
+            }
+
+        } while (day > 30);
+
+        if (month == 1) {
+            if (day > 21)
+                System.out.println("Kova Burcu");
+            else
+                System.out.println("Oğlak Burcu");
+        } else if (month == 2) {
+            if (day > 19)
+                System.out.println("Balık Burcu");
+            else
+                System.out.println("Kova Burcu");
+        } else if (month == 3) {
+            if (day > 20)
+                System.out.println("Koç Burcu");
+            else
+                System.out.println("Balık Burcu");
+        } else if (month == 4) {
+            if (day > 20)
+                System.out.println("Boğa Burcu");
+            else
+                System.out.println("Koç Burcu");
+        } else if (month == 5) {
+            if (day > 21)
+                System.out.println("İkizler Burcu");
+            else
+                System.out.println("Koç Burcu");
+        } else if (month == 6) {
+            if (day > 22)
+                System.out.println("Yengeç Burcu");
+            else
+                System.out.println("İkizler Burcu");
+        } else if (month == 7) {
+            if (day > 22)
+                System.out.println("Aslan Burcu");
+            else
+                System.out.println("Yengeç Burcu");
+        } else if (month == 8) {
+            if (day > 22)
+                System.out.println("Başak Burcu");
+            else
+                System.out.println("Aslan Burcu");
+        } else if (month == 9) {
+            if (day > 22)
+                System.out.println("Terazi Burcu");
+            else
+                System.out.println("Başak Burcu");
+        } else if (month == 10) {
+            if (day > 22)
+                System.out.println("Akrep Burcu");
+            else
+                System.out.println("Terazi Burcu");
+        } else if (month == 11) {
+            if (day > 21)
+                System.out.println("Yay Burcu");
+            else
+                System.out.println("Akrep Burcu");
+        } else if (month == 12) {
+            if (day > 21)
+                System.out.println("Oğlak Burcu");
+            else
+                System.out.println("Yay Burcu");
+        }
+
+
+    }
+}
